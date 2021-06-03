f73e19f8a36cd8d1f36dc469400dd92a236c1410
NithinAbishekRaghuraman <76937199+NithinAbishekk@users.noreply.github.com>
2021-06-03 19:35:23 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,30 @@
+const accessCode1 = "PASSWORD";
+const accessCode2 = "ACCOUNTS";
+const accessCode3 = "ANYDESK";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("W O D P S A R S", 100,50)
+    fill("lightblue")
+    text("Hint: Secret code for opening personal information !!", 100,70)
+
+// add code for displaying rest of the names and the hints.
+
+    fill("white");
+    textSize(15);
+    text("C U T N I F O N", 700,150);
+    fill("lightblue");
+    text("Hint: Stores the cash and personal informations !!", 700,170);
+
+    fill("white");
+    textSize(15);
+    text("Y A E N K D S", 100,330);
+    fill("lightblue");
+    text("Hint: Controlling the computer from anywhere !!", 100,350);
+
+   
+    
+
+}
